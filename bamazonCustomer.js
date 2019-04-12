7fbdeab0321914a9bea639c043ce5a414c57df56
Ben Shurts <bshurts93@gmail.com>
2019-04-12 13:51:27 -0500

    Setup inquirer prompt to get user input about which ID they want

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 var inquirer = require("inquirer");
 var mysql = require('mysql');
 
+var cliBr = "---------------------------------------------------------------";
+
 // MYSQL SETUP
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -9,16 +11,53 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-connection.connect();
+// MYSQL FUNCTIONS
+function seeItems() {
+    connection.connect();
 
-connection.query('SELECT * FROM bamazon.products;', function (error, results) {
-    if (error) throw error;
-
-    for (var i = 0; i < results.length; i++) {
-        console.log(results[i].product_name);
-    }
+    connection.query('SELECT * FROM bamazon.products;', function (error, results) {
+        if (error) throw error;
 
 
-    connection.end();
-});
+        for (var i = 0; i < results.length; i++) {
+            var item = results[i];
+            console.log(cliBr);
+            console.log(item.item_id + "   |   " + item.product_name + "   |   " + item.price);
+        }
+        console.log(cliBr);
 
+
+        connection.end();
+
+        chooseItem();
+    });
+}
+
+
+
+// INQUIRER PROMPTS
+function chooseItem() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "chosenID",
+            message: "Please provide the ID of the item you would like to purchase:",
+            // DOES NOT WORK, FIX LATER
+            // validate: function (input) {
+            //     if (typeof parseFloat(input) === "number") {
+            //         return true;
+            //     } else {
+            //         console.log("\r\n Please provide a valid ID number");
+            //     }
+            // }
+        }
+    ]).then(function (res) {
+        console.log("cool! you selected the item with an id of " + res.chosenID);
+    });
+}
+
+
+
+
+
+seeItems();
